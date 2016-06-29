4008f69e070847ef233c8370bba10e77bb69929f
MrWheat <390823033@qq.com>
2016-06-29 10:45:31 +0800

    第一次提交

---
+++ MZHelpCenterDemo/MZHelpCenterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MZHelpCenterDemo
+//
+//  Created by 麦子 on 16/6/29.
+//  Copyright © 2016年 麦子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
